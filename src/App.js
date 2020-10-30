67d37e67c112387faad4f696d9aca29c1637c92d
Jude Clark <judeclark2019@gmail.com>
2020-10-30 16:36:07 -0400

    building header component, installed fontawesome package

--- src/App.js
+++ src/App.js
@@ -1,20 +1,73 @@
 import logo from "./logo.svg";
+import React, { useEffect, useState, useMemo } from "react";
 import "./App.css";
+import Header from "./components/Header/index.js";
 
 const App = () => {
+  // const [comments, setComments] = useState([]);
+  // // const [loader, showLoader, hideLoader] = useFullPageLoader();
+  // const [totalItems, setTotalItems] = useState(0);
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const [search, setSearch] = useState("");
+  // const [sorting, setSorting] = useState({ field: "", order: "" });
+
+  // const ITEMS_PER_PAGE = 50;
+
+  // const headers = [
+  //   { name: "No#", field: "id", sortable: false },
+  //   { name: "Name", field: "name", sortable: true },
+  //   { name: "Email", field: "email", sortable: true },
+  //   { name: "Comment", field: "body", sortable: false },
+  // ];
+
+  // useEffect(() => {
+  //   const getData = () => {
+  //     // showLoader();
+
+  //     fetch("https://jsonplaceholder.typicode.com/comments")
+  //       .then((response) => response.json())
+  //       .then((json) => {
+  //         // hideLoader();
+  //         setComments(json);
+  //         console.log(json);
+  //       });
+  //   };
+
+  //   getData();
+  // }, []);
+
+  // const commentsData = useMemo(() => {
+  //   let computedComments = comments;
+
+  //   if (search) {
+  //     computedComments = computedComments.filter(
+  //       (comment) =>
+  //         comment.name.toLowerCase().includes(search.toLowerCase()) ||
+  //         comment.email.toLowerCase().includes(search.toLowerCase())
+  //     );
+  //   }
+
+  //   setTotalItems(computedComments.length);
+
+  //   //Sorting comments
+  //   if (sorting.field) {
+  //     const reversed = sorting.order === "asc" ? 1 : -1;
+  //     computedComments = computedComments.sort(
+  //       (a, b) => reversed * a[sorting.field].localeCompare(b[sorting.field])
+  //     );
+  //   }
+
+  //   //Current Page slice
+  //   return computedComments.slice(
+  //     (currentPage - 1) * ITEMS_PER_PAGE,
+  //     (currentPage - 1) * ITEMS_PER_PAGE + ITEMS_PER_PAGE
+  //   );
+  // }, [comments, currentPage, search, sorting]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Edit successful</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
