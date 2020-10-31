051230cf3784a0a8b59d5fc5fe003361e756df59
Jude Clark <judeclark2019@gmail.com>
2020-10-31 13:31:41 -0400

    added sorting icons

--- src/components/SortingTable.js
+++ src/components/SortingTable.js
@@ -32,6 +32,13 @@ const SortingTable = () => {
               {headerGroup.headers.map((column) => (
                 <th {...column.getHeaderProps(column.getSortByToggleProps())}>
                   {column.render("Header")}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? " 🔽"
+                        : " 🔼"
+                      : ""}
+                  </span>
                 </th>
               ))}
             </tr>
