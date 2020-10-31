270322396c056ca9f288e81b479bf3f3fbabc06a
Jude Clark <judeclark2019@gmail.com>
2020-10-31 17:53:56 -0400

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# User Data Table
-
-https://judeclark19.github.io/user-data-table
-
-https://www.youtube.com/playlist?list=PLC3y8-rFHvwgWTSrDiwmUsl4ZvipOw9Cz
--- src/components/Blurb.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Blurb = () => {
-  return <div id="blurb">Click on any column header to sort.</div>;
-};
-
-export default Blurb;
