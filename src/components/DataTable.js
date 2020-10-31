ad5f84596e4297a39e3adcf700c9fc8621d794f9
Jude Clark <judeclark2019@gmail.com>
2020-10-31 13:56:42 -0400

    implemented search function :

---
+++ src/components/DataTable.js
@@ -0,0 +1,75 @@
+import React, { useMemo } from "react";
+import { useTable, useSortBy, useGlobalFilter } from "react-table";
+import my_data from "./my_data.json";
+import { COLUMNS } from "./columns";
+import GlobalFilter from "./GlobalFilter";
+
+const DataTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => my_data, []);
+
+  const tableInstance = useTable(
+    {
+      columns: columns,
+      data: data,
+    },
+    useGlobalFilter,
+    useSortBy
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    setGlobalFilter,
+  } = tableInstance;
+
+  const { globalFilter } = state;
+
+  return (
+    <>
+      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+      <div>
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                    {column.render("Header")}
+                    <span>
+                      {column.isSorted
+                        ? column.isSortedDesc
+                          ? " 🔽"
+                          : " 🔼"
+                        : ""}
+                    </span>
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default DataTable;
